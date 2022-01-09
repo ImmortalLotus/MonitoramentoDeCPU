7ebb4eb894ed12dece48c10929c569d84816dbdd
ImmortalLotus <72933183+ImmortalLotus@users.noreply.github.com>
2022-01-09 14:22:47 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace MonitoramentoDeCPUaLaTaskManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) { 
+                try
+                {
+
+                    ManagementObjectSearcher searcher =
+                        new ManagementObjectSearcher("root\\CIMV2",
+                        "SELECT * FROM Win32_PerfFormattedData_PerfProc_Process");
+
+
+                    foreach (ManagementObject queryObj in searcher.Get())
+                    {
+
+
+                        Console.WriteLine("ProcessID: {0}", queryObj["IDProcess"]);
+                        Console.WriteLine("Name: {0}", queryObj["Name"]);
+                        Console.WriteLine("Handles: {0}", queryObj["HandleCount"]);
+                        Console.WriteLine("Threads: {0}", queryObj["ThreadCount"]);
+                        Console.WriteLine("Memory: {0}", queryObj["WorkingSetPrivate"]);
+                        Console.WriteLine("CPU%: {0}", queryObj["PercentProcessorTime"]);
+                    }
+
+
+                }
+                catch (ManagementException e)
+                {
+                    MessageBox.Show("An error occurred while querying for WMI data: " + e.Message);
+                }  
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
