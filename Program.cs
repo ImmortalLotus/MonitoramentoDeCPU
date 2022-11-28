00d494f220bbb139f157edc1478b72e2d1166f4c
Joao Thomas de Souza Telles <joao.telles@setic.ro.gov.br>
2022-11-28 16:07:22 -0300

    é isso mesmo

--- Program.cs
+++ Program.cs
@@ -7,42 +7,26 @@ using System.Text;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
-namespace MonitoramentoDeCPUaLaTaskManager
+namespace MonitoramentoDeCPUeRAM
 {
     class Program
     {
         static void Main(string[] args)
         {
-            while (true) { 
-                try
-                {
+            PerformanceCounter cpuCounter;
+            PerformanceCounter ramCounter;
 
-                    ManagementObjectSearcher searcher =
-                        new ManagementObjectSearcher("root\\CIMV2",
-                        "SELECT * FROM Win32_PerfFormattedData_PerfProc_Process");
+            cpuCounter = new PerformanceCounter("Processor","% Processor Time","_Total", "CARINE-VAIO");
 
+            cpuCounter.NextValue();
 
-                    foreach (ManagementObject queryObj in searcher.Get())
-                    {
+            System.Threading.Thread.Sleep(1000);// 1 second wait
 
-
-                        Console.WriteLine("ProcessID: {0}", queryObj["IDProcess"]);
-                        Console.WriteLine("Name: {0}", queryObj["Name"]);
-                        Console.WriteLine("Handles: {0}", queryObj["HandleCount"]);
-                        Console.WriteLine("Threads: {0}", queryObj["ThreadCount"]);
-                        Console.WriteLine("Memory: {0}", queryObj["WorkingSetPrivate"]);
-                        Console.WriteLine("CPU%: {0}", queryObj["PercentProcessorTime"]);
-                    }
-
-
-                }
-                catch (ManagementException e)
-                {
-                    MessageBox.Show("An error occurred while querying for WMI data: " + e.Message);
-                }  
-                Thread.Sleep(1000);
-            }
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            Console.WriteLine("CPU : "+cpuCounter.NextValue()+"%");
+            Console.WriteLine("RAM : " + ramCounter.NextValue()+"MB");
         }
     }
 }
